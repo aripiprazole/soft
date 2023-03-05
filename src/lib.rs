b387a5d66eca370633ab358cbe08fa6f1745c2a5
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-03-05 11:41:07 -0300

    feat: implement eq, partial, eq and debug for ast

--- src/lib.rs
+++ src/lib.rs
@@ -8,33 +8,27 @@ lalrpop_mod!(pub soft);
 #[cfg(test)]
 mod tests {
     use super::*;
-    use runtime::{Value, ValueRef};
+    use runtime::ValueRef;
 
     #[test]
     fn test_parser_num() {
         let result = soft::TermParser::new().parse("1").unwrap();
 
-        assert_eq!(result.to_string(), ValueRef::new_num(1).to_string());
+        assert_eq!(result, ValueRef::new_num(1));
     }
 
     #[test]
     fn test_parser_atom() {
         let result = soft::TermParser::new().parse("foo").unwrap();
 
-        assert_eq!(
-            result.to_string(),
-            ValueRef::new(Value::Atom("foo".to_string())).to_string()
-        );
+        assert_eq!(result, ValueRef::atom("foo".to_string()));
     }
 
     #[test]
     fn test_parser_quote() {
         let result = soft::TermParser::new().parse("'foo").unwrap();
 
-        assert_eq!(
-            result.to_string(),
-            ValueRef::new(Value::Quote(ValueRef::new(Value::Atom("foo".to_string())))).to_string()
-        );
+        assert_eq!(result, ValueRef::quote(ValueRef::atom("foo".to_string())));
     }
 
     #[test]
@@ -42,12 +36,11 @@ mod tests {
         let result = soft::TermParser::new().parse("(1 2)").unwrap();
 
         assert_eq!(
-            result.to_string(),
-            ValueRef::new(Value::Cons(
+            result,
+            ValueRef::cons(
                 ValueRef::new_num(1),
-                ValueRef::new(Value::Cons(ValueRef::new_num(2), ValueRef::new(Value::Nil)))
-            ))
-            .to_string()
+                ValueRef::cons(ValueRef::new_num(2), ValueRef::nil())
+            )
         );
     }
 }
