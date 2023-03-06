045c6e19b6f29d916756fc740d503c1bed079c24
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-03-06 15:28:32 -0300

    refactor: isolate execution and types
    Co-authored-by: Chiyoku <algebraic.sofia+github@gmail.com>

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,7 +1,4 @@
-use std::{
-    ffi::{c_void, CStr},
-    mem,
-};
+use std::ffi::{c_void, CStr};
 
 use llvm_sys::{
     analysis::{LLVMVerifierFailureAction::LLVMReturnStatusAction, LLVMVerifyModule},
@@ -9,9 +6,14 @@ use llvm_sys::{
     target::{LLVM_InitializeNativeAsmPrinter, LLVM_InitializeNativeTarget},
     LLVMDiagnosticSeverity::{LLVMDSError, LLVMDSNote, LLVMDSRemark, LLVMDSWarning},
 };
-pub use llvm_sys::{core::*, execution_engine::*, prelude::*};
 
 use crate::util::{cstr, llvm_wrapper};
+
+pub use llvm_sys::{core::*, execution_engine::*, prelude::*};
+
+pub mod compile;
+pub mod execution;
+pub mod types;
 
 pub type CodegenError = String;
 
@@ -19,7 +21,7 @@ pub struct Codegen {
     pub context: LLVMContextRef,
     pub module: LLVMModuleRef,
     pub builder: LLVMBuilderRef,
-    pub types: Types,
+    pub types: types::Types,
 }
 
 impl Drop for Codegen {
@@ -37,7 +39,7 @@ impl Codegen {
         let context = LLVMContextCreate();
         let module = LLVMModuleCreateWithNameInContext(cstr!("soft"), context);
         let builder = LLVMCreateBuilderInContext(context);
-        let types = Types::try_new(context)?;
+        let types = types::Types::try_new(context)?;
 
         Ok(Codegen {
             context,
@@ -56,7 +58,7 @@ impl Codegen {
     }
 
     pub unsafe fn verify_module(&self) -> Result<(), String> {
-        let mut err = mem::zeroed();
+        let mut err = std::mem::zeroed();
 
         if LLVMVerifyModule(self.module, LLVMReturnStatusAction, &mut err) == 1 {
             let message = CStr::from_ptr(err).to_string_lossy().to_string();
@@ -93,54 +95,6 @@ pub extern "C" fn handle_diagnostic(info: LLVMDiagnosticInfoRef, _context: *mut 
     }
 }
 
-llvm_wrapper!(Type, LLVMTypeRef, LLVMPrintTypeToString);
-llvm_wrapper!(Value, LLVMValueRef, LLVMPrintValueToString);
-
-pub struct Types {
-    pub void_ptr: LLVMTypeRef,
-    pub u64: LLVMTypeRef,
-}
-
-impl Types {
-    pub unsafe fn try_new(context: LLVMContextRef) -> Result<Self, CodegenError> {
-        Ok(Self {
-            void_ptr: LLVMVoidTypeInContext(context),
-            u64: LLVMInt64TypeInContext(context),
-        })
-    }
-}
-
-pub struct ExecutionEngine(pub *mut LLVMOpaqueExecutionEngine);
-
-impl Drop for ExecutionEngine {
-    fn drop(&mut self) {
-        // TODO
-        // unsafe {
-        //     LLVMDisposeExecutionEngine(self.0);
-        // }
-    }
-}
-
-impl ExecutionEngine {
-    pub unsafe fn try_new(module: LLVMModuleRef) -> Result<Self, CodegenError> {
-        let mut ptr = mem::MaybeUninit::uninit();
-        let mut err = mem::zeroed();
-
-        if LLVMCreateExecutionEngineForModule(ptr.as_mut_ptr(), module, &mut err) != 0 {
-            // In case of error, we must avoid using the uninitialized ExecutionEngineRef.
-            assert!(!err.is_null());
-            let err = CStr::from_ptr(err);
-            return Err(format!("Failed to create execution engine: {:?}", err));
-        }
-
-        Ok(ExecutionEngine(ptr.assume_init()))
-    }
-
-    pub unsafe fn get_function_address(&self, name: &str) -> u64 {
-        LLVMGetFunctionAddress(self.0, format!("{name}\0").as_ptr() as *const i8)
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -174,12 +128,12 @@ mod tests {
                 panic!("Module verification failed")
             });
 
-            let engine = ExecutionEngine::try_new(codegen.module).unwrap();
+            let engine = execution::ExecutionEngine::try_new(codegen.module).unwrap();
 
             let f: extern "C" fn(u64, u64) -> u64 =
                 std::mem::transmute(engine.get_function_address("sum"));
 
-            println!("sum(1, 2): {} = {}", Type(sum_t), f(1, 2));
+            println!("sum(1, 2): {} = {}", types::Type(sum_t), f(1, 2));
         }
     }
 }
---
+++ src/codegen/execution.rs
@@ -0,0 +1,32 @@
+use super::*;
+
+pub struct ExecutionEngine(pub *mut LLVMOpaqueExecutionEngine);
+
+impl Drop for ExecutionEngine {
+    fn drop(&mut self) {
+        // TODO
+        // unsafe {
+        //     LLVMDisposeExecutionEngine(self.0);
+        // }
+    }
+}
+
+impl ExecutionEngine {
+    pub unsafe fn try_new(module: LLVMModuleRef) -> Result<Self, CodegenError> {
+        let mut ptr = std::mem::MaybeUninit::uninit();
+        let mut err = std::mem::zeroed();
+
+        if LLVMCreateExecutionEngineForModule(ptr.as_mut_ptr(), module, &mut err) != 0 {
+            // In case of error, we must avoid using the uninitialized ExecutionEngineRef.
+            assert!(!err.is_null());
+            let err = CStr::from_ptr(err);
+            return Err(format!("Failed to create execution engine: {:?}", err));
+        }
+
+        Ok(ExecutionEngine(ptr.assume_init()))
+    }
+
+    pub unsafe fn get_function_address(&self, name: &str) -> u64 {
+        LLVMGetFunctionAddress(self.0, format!("{name}\0").as_ptr() as *const i8)
+    }
+}
