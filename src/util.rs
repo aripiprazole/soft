119109306aa7ffcc9419cababb94576355fedbf8
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-03-06 14:04:39 -0300

    style: move macros to end of `util.rs` file

--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,42 @@
 use std::fmt::Display;
 
-macro_rules! bool_enum {
-    ($name:ident) => {
-        #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-        pub enum $name {
-            Yes,
-            No,
+pub trait Container {
+    type Target;
+
+    fn f(&self) -> Self::Target;
+}
+
+pub enum Mode {
+    Interperse,
+    Before,
+}
+
+pub struct Spaced<'a, T>(pub Mode, pub &'static str, pub &'a [T])
+where
+    T: Display;
+
+impl<'a, T> Display for Spaced<'a, T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Spaced(Mode::Interperse, string, slice) => {
+                if !slice.is_empty() {
+                    write!(f, "{}", slice[0])?;
+                    for element in &slice[1..] {
+                        write!(f, "{string}{element}")?;
+                    }
+                }
+            }
+            Spaced(Mode::Before, string, slice) => {
+                for element in slice.iter() {
+                    write!(f, "{string}{element}")?;
+                }
+            }
         }
-    };
+        Ok(())
+    }
 }
 
 macro_rules! cstr {
@@ -16,6 +45,16 @@ macro_rules! cstr {
     };
     () => {
         "\0".as_ptr() as *const i8
+    };
+}
+
+macro_rules! bool_enum {
+    ($name:ident) => {
+        #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+        pub enum $name {
+            Yes,
+            No,
+        }
     };
 }
 
@@ -59,42 +98,3 @@ macro_rules! llvm_wrapper {
 pub(crate) use bool_enum;
 pub(crate) use cstr;
 pub(crate) use llvm_wrapper;
-
-pub trait Container {
-    type Target;
-
-    fn f(&self) -> Self::Target;
-}
-
-pub enum Mode {
-    Interperse,
-    Before,
-}
-
-pub struct Spaced<'a, T>(pub Mode, pub &'static str, pub &'a [T])
-where
-    T: Display;
-
-impl<'a, T> Display for Spaced<'a, T>
-where
-    T: Display,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Spaced(Mode::Interperse, string, slice) => {
-                if !slice.is_empty() {
-                    write!(f, "{}", slice[0])?;
-                    for element in &slice[1..] {
-                        write!(f, "{string}{element}")?;
-                    }
-                }
-            }
-            Spaced(Mode::Before, string, slice) => {
-                for element in slice.iter() {
-                    write!(f, "{string}{element}")?;
-                }
-            }
-        }
-        Ok(())
-    }
-}
