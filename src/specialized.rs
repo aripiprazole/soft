d828076b94e235c80893d97963d06a0729d5ddab
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-03-06 09:04:15 -0300

    refactor: move Spaced to util

--- src/specialized.rs
+++ src/specialized.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 
 use crate::{
     runtime::{Value, ValueRef},
-    util::bool_enum,
+    util::{bool_enum, Mode, Spaced},
 };
 
 bool_enum!(IsMacro);
@@ -22,39 +22,6 @@ pub enum Term {
     Num(u64),
     Quote(Box<Term>),
     Nil,
-}
-
-pub enum Mode {
-    Interperse,
-    Before,
-}
-
-pub struct Spaced<'a, T>(Mode, &'static str, &'a [T])
-where
-    T: Display;
-
-impl<'a, T> Display for Spaced<'a, T>
-where
-    T: Display,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Spaced(Mode::Interperse, string, slice) => {
-                if !slice.is_empty() {
-                    write!(f, "{}", slice[0])?;
-                    for element in &slice[1..] {
-                        write!(f, "{string}{element}")?;
-                    }
-                }
-            }
-            Spaced(Mode::Before, string, slice) => {
-                for element in slice.iter() {
-                    write!(f, "{string}{element}")?;
-                }
-            }
-        }
-        Ok(())
-    }
 }
 
 impl Display for Term {
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 macro_rules! bool_enum {
     ($name:ident) => {
         #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -9,3 +11,36 @@ macro_rules! bool_enum {
 }
 
 pub(crate) use bool_enum;
+
+pub enum Mode {
+    Interperse,
+    Before,
+}
+
+pub struct Spaced<'a, T>(pub Mode, pub &'static str, pub &'a [T])
+where
+    T: Display;
+
+impl<'a, T> Display for Spaced<'a, T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Spaced(Mode::Interperse, string, slice) => {
+                if !slice.is_empty() {
+                    write!(f, "{}", slice[0])?;
+                    for element in &slice[1..] {
+                        write!(f, "{string}{element}")?;
+                    }
+                }
+            }
+            Spaced(Mode::Before, string, slice) => {
+                for element in slice.iter() {
+                    write!(f, "{string}{element}")?;
+                }
+            }
+        }
+        Ok(())
+    }
+}
