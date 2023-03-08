8510acddf9687c717942340b1f0a7de48f3a9ce7
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-03-08 14:04:03 -0300

    refactor: extract type macros

--- src/codegen/helpers.rs
+++ src/codegen/helpers.rs
@@ -3,10 +3,12 @@ use super::*;
 pub mod builder;
 pub mod context;
 pub mod module;
+pub mod types;
 
 pub use builder::*;
 pub use context::*;
 pub use module::*;
+pub use types::*;
 
 impl Codegen {
     pub fn make_int_const(&self, value: u64) -> LLVMValueRef {
@@ -21,27 +23,3 @@ impl Codegen {
         self.make_int_const(1)
     }
 }
-
-macro_rules! pointer_type {
-    ($e:expr) => {
-        unsafe { LLVMPointerType($e, 0) }
-    };
-}
-
-macro_rules! function_type {
-    ($return_type:expr, $arg_type:expr; $n:expr) => {{
-        let mut args_types = vec![$arg_type; $n];
-        unsafe {
-            LLVMFunctionType($return_type, args_types.as_mut_ptr(), args_types.len() as u32, 0)
-        }
-    }};
-    ($return_type:expr, $($args:expr),+ $(,)?) => {{
-        let mut args_types = [$($args),+];
-        unsafe {
-            LLVMFunctionType($return_type, args_types.as_mut_ptr(), args_types.len() as u32, 0)
-        }
-    }};
-}
-
-pub(crate) use function_type;
-pub(crate) use pointer_type;
